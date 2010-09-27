20cf6d3624da8f71840bd7b122a7f9303f529a76
rcheze@gmail.com <rcheze@gmail.com@5ee61bb3-37e8-a77b-29e4-26c481578d6d>
2010-09-27 10:12:57 +0000

    code cleaned

--- trunk/ timeattack/src/com/game/timeattack/provider/TimeAttack.java
+++ trunk/ timeattack/src/com/game/timeattack/provider/TimeAttack.java
@@ -61,7 +61,7 @@ public class TimeAttack {
 		/**
 		 * The default sort order for this table
 		 */
-		public static final String DEFAULT_SORT_ORDER = "launch_time DESC";
+		public static final String DEFAULT_SORT_ORDER = "launch_time ASC";
 
 		/**
 		 * The MIME type of {@link #CONTENT_URI} providing a directory of
